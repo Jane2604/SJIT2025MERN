59e76edbc16ce7e6a86dc1789df762a7eb1bc703
afra <janeafra2604@gmail.com>
2025-02-17 12:22:24 +0530

    sevenfn

--- JavaScript/es6.js
+++ JavaScript/es6.js
@@ -1,6 +1,6 @@
 //1. variables - refer declarations
 //2. arrow function
-
+/*
 function testFunction()
 {
     console.log("This is a test function");
@@ -33,3 +33,45 @@ console.log(c);  // 40
 console.log(f);  // 35
 a=10
 console.log((a%2)?"Odd":"Even");  // 1:0
+
+// Spread operator(...)
+console.log("Spread operator");
+Stud1year=["user1","user2","user3"]
+Stud2year=["user4","user5","user6"]
+Stud3year=["user7","user8","user9"]
+Stud4year=["user10","user11","user12"]
+studDB=[...Stud1year,...Stud2year,...Stud3year,...Stud4year]
+console.log(studDB);
+alumniDB=Stud4year
+console.log(alumniDB)
+
+// Rest operator(...)
+console.log("Rest operator");
+function studentDB(...studDataBase)
+{
+    console.log(studDataBase);
+}
+studentDB(studDB)
+
+// Destructing operator
+var array=[10,20,30,40,50,60]
+var [a,b,c,d,e,f]=array
+console.log(a,b,c,d,e,f);
+
+
+// Hoisting
+console.log(a);  
+var a=10;
+console.log(a);
+*/
+funhoisting()
+function funhoisting()
+{
+    console.log("Checking Functional hoisting");
+}
+
+/*funhoisting=()=>
+{
+    console.log("Checking Functional hoisting");
+}
+funhoisting*/
