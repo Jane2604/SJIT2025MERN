d4cc165fd218e94e8d13f667cebba72753ba7b93
afra <janeafra2604@gmail.com>
2025-02-17 14:42:43 +0530

    sevenan

--- JavaScript/callback.js
+++ JavaScript/callback.js
@@ -8,3 +8,20 @@ function callbackSJIT()
     console.log("Welcome back to SJIT");
 }
 SJITCollege("Welcome to SJIT",callbackSJIT)
+function formsubmission(message,cbFun)
+{
+    if(cbFun())
+    {
+        console.log(message);
+    }
+    else
+    {
+        console.log("Form submission unsuccessful");
+    }
+}
+function formvalidation()
+{
+    console.log("Form validation successful");
+    return true
+}
+formsubmission("Form submission successful",formvalidation)
