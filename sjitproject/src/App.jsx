9fcbc9207b2ed10860fcb3166422805da652d4e9
afra <janeafra2604@gmail.com>
2025-02-19 11:54:02 +0530

    eightfn

--- sjitproject/src/App.jsx
+++ sjitproject/src/App.jsx
@@ -1,13 +1,26 @@
-import Home from './components/FunctionalComponents/Home.jsx'
+import Home from './components/FunctionalComponents/Home.jsx';
+import ClassCompEG from './components/ClassComponents/ClassCompEG.jsx';
+import About from './components/FunctionalComponents/About.jsx';
+import Contact from './components/FunctionalComponents/Contact.jsx';
+import Gallery from './components/FunctionalComponents/Gallery.jsx'; 
+import Login from './components/FunctionalComponents/Login.jsx'; 
+import Navbar from './components/FunctionalComponents/Navbar.jsx';
 function App() {
-return (
-      <div>
-        <h1>Welcome to react</h1>
-        <p>Hello<br />SJIT</p>
-        <hr></hr>
-        <home></home>
-      </div>
-  )
+  return (
+    <div>
+      <Navbar />
+      <ClassCompEG />
+      <hr />
+      <Home properties="Hello" sjit="SJIT"  />
+      <hr />
+      <About />
+      <hr />
+      <Gallery />
+      <hr />
+      <Contact />
+      <hr />
+      <Login />
+    </div>
+  );
 }
-
-export default App
+export default App;
