3c1fb644044c2d2b322e8165e54e8091970d6fba
afra <janeafra2604@gmail.com>
2025-02-20 14:08:11 +0530

    tenan

--- sjitproject/src/components/FunctionalComponents/Hooks/UseMemo.jsx
+++ sjitproject/src/components/FunctionalComponents/Hooks/UseMemo.jsx
@@ -7,16 +7,23 @@ const UseMemo=()=>{
     var[number,setNumber]=useState(0);
     var[theme,setTheme]=useState(false);
     var styling={
+        backgroundColor:theme?"palevioletred":"white",
+        color:theme?"white":"palevioletred",
+    };
+   /* var styling=useMemo(()=>{
         backgroundColor:theme?"black":"white",
         color:theme?"white":"black",
-    };
+    })
+   var doublingUpANumber=useMemo(()=>{
+    return slowFunction(number)
+   },[number])*/
     return(
         <div style={styling}>
-        <button onClick={()=>setTheme(!theme)}>Toggle Theme</button>
             <h1>This is UseMemo example</h1>
             Number Box:{" "} <input type="number" value={number} onChange={(e)=> setNumber(e.target.value)}/>
             <h2>The number is {number}</h2>
             <h2>The number is {slowFunction(number)}</h2>
+            <button onClick={()=>setTheme(!theme)}>Toggle Theme</button>
         </div>
     )
 }
